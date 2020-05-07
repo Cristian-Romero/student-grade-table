fa9941e2ce17ba027198b6c770c697c987da0c32
Cristian Romero <crisromero02@outlook.com>
2020-05-07 16:55:09 -0700

    Created a query selector for an element of type form and assinged it a variable named formEl. I then instantiated the GradeForm passing the variable formEl as an argument and assigned the return value to a variable named enterGrade. Finally I added enterGrade as a third arguement to the instantiated App

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var tableEl = document.getElementById('grade-table');
 var headerEl = document.getElementById('header');
+var formEl = document.querySelector('form');
+var enterGrade = new GradeForm(formEl);
 var updateHeader = new PageHeader(headerEl);
 var summonGrades = new GradeTable(tableEl);
-var fetchGrades = new App(summonGrades, updateHeader);
+var fetchGrades = new App(summonGrades, updateHeader, enterGrade);
 fetchGrades.start();
