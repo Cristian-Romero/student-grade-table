dcf2534657144ccf21734d4676d8e5c14895cca8
Cristian Romero <crisromero02@outlook.com>
2020-05-07 12:56:09 -0700

    Added updateHeader as an argument to the instantiated App variable

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var tableEl = document.getElementById('grade-table');
 var headerEl = document.getElementById('header');
 var updateHeader = new PageHeader(headerEl);
 var summonGrades = new GradeTable(tableEl);
-var fetchGrades = new App(summonGrades);
+var fetchGrades = new App(summonGrades, updateHeader);
 fetchGrades.start();
