55f792fa21cb0a767a6b371125160fbc2aacd3a4
Cristian Romero <crisromero02@outlook.com>
2020-05-06 17:48:34 -0700

    Created App class inside app.js that gets grades for students. Also instantiated App class in the main.js file and called the start method off that object

---
+++ components/app.js
@@ -0,0 +1,26 @@
+class App{
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+  constructor(){
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  getGrades() {
+    $.ajax({
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      method: "GET",
+      headers: {
+        "X-Access-Token": "8sfZiJo4"
+      },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+    })
+  }
+  start() {
+    this.getGrades();
+  }
+}
---
+++ main.js
@@ -0,0 +1,2 @@
+var fetchGrades = new App;
+fetchGrades.start();
