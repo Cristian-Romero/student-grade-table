bce5bc600b95a151683f47f373c36bf461b49e74
Cristian Romero <crisromero02@outlook.com>
2020-05-08 11:29:07 -0700

    Created new method named renderGradeRow

--- components/grade-table.js
+++ components/grade-table.js
@@ -26,4 +26,31 @@ class GradeTable{
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade;
   }
+
+  renderGradeRow(data, deleteGrade) {
+    var tRow = document.createElement('tr');
+    var sName = document.createElement('td');
+    var cName = document.createElement('td');
+    var sGrade = document.createElement('td');
+    var delOp = document.createElement('td');
+    var delButt = document.createElement('button');
+
+    sName.textContent = data.name;
+    cName.textContent = data.course;
+    sGrade.textContent = data.grade;
+    delButt.textContent = 'DELETE';
+    delButt.className = 'btn btn-danger';
+
+    delOp.appendChild(delButt);
+
+    tRow.appendChild(sName);
+    tRow.appendChild(cName);
+    tRow.appendChild(sGrade);
+    tRow.appendChild(delOp);
+
+    delButt.addEventListener('click', function(event) {
+      deleteGrade(data.id);
+    })
+    return tRow;
+  }
 }
