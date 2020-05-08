6214759139f14f2611e8047a32ba9659111ffad9
Cristian Romero <crisromero02@outlook.com>
2020-05-07 17:19:44 -0700

    Reformated addEventListener on this.Element

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,13 +1,19 @@
 class GradeForm {
   constructor(formElement) {
-    this.handleSubmit = this.handleSubmit.bind(this)
     this.formElement = formElement;
+    this.formElement.addEventListener('submit', this.handleSubmit.bind(this))
   }
   onSubmit(createGrade) {
     this.createGrade = createGrade;
   }
   handleSubmit(event) {
     event.preventDefault();
+    var getFormData = new FormData(event.target);
+    var name = getFormData.get('name');
+    var course = getFormData.get('course');
+    var grade = getFormData.get('grade');
+    this.createGrade(name, course, grade);
+    event.target.reset();
     console.log('hi');
   }
 }
